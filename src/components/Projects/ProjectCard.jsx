21cbb38ad196fe60d81f6c15e0e3b9d8a56800ec
isabellaterano <teranoiisabella@gmail.com>
2024-05-12 23:34:51 -0300

    add project

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,11 +1,10 @@
 import { useTranslation } from "react-i18next";
 import { motion } from "framer-motion";
-import realEstate from "../../assets/images/realestate.png";
-import ecommerce from "../../assets/images/ecommercechair.png";
+import realEstate from "../../assets/images/projectrealestate.png";
+import ecommerce from "../../assets/images/projectchair.png";
 import qrcode from "../../assets/images/qrcode.png";
-import landingPage from "../../assets/images/landingpage.png";
-import extension from "../../assets/images/extension.png";
-import stayHealthy from "../../assets/images/stayhealthy.png";
+import digitalagency from "../../assets/images/projectdigitalagency.png";
+import stayHealthy from "../../assets/images/projectstayhealthy.png";
 import "./ProjectCard.css";
 
 const ProjectCard = () => {
@@ -95,7 +94,8 @@ const ProjectCard = () => {
           <h3>{t("productcard.title.chair")}</h3>
           <p>{t("productcard.description.chair")}</p>
           <span>
-            React - Express - MongoDB - Node - Stripe - Tailwind - daisyUI
+            React - Javascript - Express - MongoDB - Node - Stripe - Tailwind -
+            daisyUI - Three.js
           </span>
           <div className="productcard__button">
             <a
@@ -108,6 +108,40 @@ const ProjectCard = () => {
             </a>
             <a
               href="https://github.com/isabellaterano/ecommerce-chair"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="productcard__btn"
+            >
+              Source
+            </a>
+          </div>
+        </motion.div>
+      </div>
+      <div className="wrapper">
+        <motion.div
+          className="productcard-img"
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5 }}
+        >
+          <img src={digitalagency} alt="landing page digital agency" />
+        </motion.div>
+
+        <motion.div className="productcard__description">
+          <h3>{t("productcard.title.landing")}</h3>
+          <p>{t("productcard.description.landing")}</p>
+          <span>React - Javascript - Tailwind - Three.js</span>
+          <div className="productcard__button">
+            <a
+              href="https://digitalagency-forge.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="productcard__btn"
+            >
+              Demo
+            </a>
+            <a
+              href="https://github.com/isabellaterano/digital-agency"
               target="_blank"
               rel="noopener noreferrer"
               className="productcard__btn"
@@ -142,66 +176,6 @@ const ProjectCard = () => {
             </a>
             <a
               href="https://github.com/isabellaterano/qr-code"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="productcard__btn"
-            >
-              Source
-            </a>
-          </div>
-        </motion.div>
-      </div>
-      <div className="wrapper">
-        <motion.div
-          className="productcard-img"
-          whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: -100 }}
-          transition={{ duration: 0.5 }}
-        >
-          <img src={landingPage} alt="landing page museum" />
-        </motion.div>
-
-        <motion.div className="productcard__description">
-          <h3>{t("productcard.title.landing")}</h3>
-          <p>{t("productcard.description.landing")}</p>
-          <span>HTML - SCSS - Javascript</span>
-          <div className="productcard__button">
-            <a
-              href="https://landingpage-museum.onrender.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="productcard__btn"
-            >
-              Demo
-            </a>
-            <a
-              href="https://github.com/isabellaterano/landingpage-museum"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="productcard__btn"
-            >
-              Source
-            </a>
-          </div>
-        </motion.div>
-      </div>
-      <div className="wrapper">
-        <motion.div
-          className="productcard-img"
-          whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: -100 }}
-          transition={{ duration: 0.5 }}
-        >
-          <img src={extension} alt="" />
-        </motion.div>
-
-        <motion.div className="productcard__description">
-          <h3>{t("productcard.title.extension")}</h3>
-          <p>{t("productcard.description.extension")}</p>
-          <span>Javascript - CSS</span>
-          <div className="productcard__button">
-            <a
-              href="https://github.com/isabellaterano/focusguard-extension"
               target="_blank"
               rel="noopener noreferrer"
               className="productcard__btn"
